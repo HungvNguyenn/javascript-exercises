346e51e40c75f72360f9c30339508bd9723abf69
Hung Nguyen <hungnguyen210703@gmail.com>
2025-08-07 17:43:36 -0700

    finish 06-11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+const divisibleByFour = year % 4 == 0; 
+const isCentury = year % 100 == 0;
+const dvisibleByFourHundred = year % 400 == 0;
+
+if(divisibleByFour && !isCentury || dvisibleByFourHundred) {
+    return true;
+} else {
+    return false;
+}
 
 };
 
